40f64601ee7a07b7236f8d0acb4c796b35bd47e2
Lisa Schulzke <56436771+lisaschulzke@users.noreply.github.com>
2021-01-02 19:56:35 +0100

    :sparkles: updating home

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:animated_background/animated_background.dart';
+import 'package:animated_background/particles.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,21 +13,29 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        primarySwatch: Colors.blue,
-        primaryColor: Colors.amberAccent,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        brightness: Brightness.light),
+          // This is the theme of your application.
+          primarySwatch: Colors.blue,
+          primaryColor: Colors.amberAccent,
+          buttonTheme: ButtonThemeData(
+            textTheme: ButtonTextTheme.primary,
+            buttonColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+          textTheme: TextTheme(
+            headline1: TextStyle(
+                fontSize: 32, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          brightness: Brightness.light),
       home: Home(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class Home extends StatefulWidget {
-  Home({
-    Key key,
-    this.title
-  }): super(key: key);
+  Home({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -42,7 +52,8 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State < Home > {
+class _HomeState extends State<Home> with TickerProviderStateMixin {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   // int _counter = 0;
 
   // void _incrementCounter() {
@@ -67,55 +78,234 @@ class _HomeState extends State < Home > {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the Home object that was created by
-
-        // the App.build method, and use it to set our appbar title.
-        title: Text("Hallo."),
-        backgroundColor: Colors.blue,
-        actions: [
-          IconButton(
+        key: _scaffoldKey,
+        backgroundColor: Color(0xffC0C5CD),
+        appBar: AppBar(
+          leading: IconButton(
             icon: Icon(Icons.menu),
-            onPressed: () {
-              print("clicked!");
-            })
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            ListTile(
-              title: Text("Home"),
-              onTap: () {
-                // Navigation
-              }),
-            ListTile(
-              title: Text("Scan"),
-              onTap: () {
-                // Navigation to scan page
-              },
-            ),
-            ListTile(
-              title: Text("Journal"),
-              onTap: () {
-                // Navigatiopn to detail? is this necessary?
-              },
-            )
-          ],
+            onPressed: () => _scaffoldKey.currentState.openDrawer(),
+            color: Colors.white,
+          ),
+          // Here we take the value from the Home object that was created by
+
+          // the App.build method, and use it to set our appbar title.
+          title: Text(
+            "Hallo",
+            style: Theme.of(context).textTheme.headline1,
+          ),
+          backgroundColor: Color(0xff677792),
+          elevation: 0,
         ),
-      ),
-      body: Stack(
-        children: [
-          Positioned(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.blue,
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              ListTile(
+                  title: Text("Home"),
+                  onTap: () {
+                    // Navigation
+                  }),
+              ListTile(
+                title: Text("Scan"),
+                onTap: () {
+                  // Navigation to scan page
+                },
               ),
-              height: 50,
-              width: 50,
+              ListTile(
+                title: Text("Journal"),
+                onTap: () {
+                  // Navigation to detail? is this necessary?
+                },
+              )
+            ],
+          ),
+        ),
+        body: Stack(children: [
+          Positioned(
+            top: 0,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.2,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.vertical(
+                    bottom: Radius.elliptical(900.5, 60.9)),
+                color: Color(0xffC677792),
+              ),
             ),
           ),
-        ],
-      ));
+          Positioned(
+            top: 10,
+            width: MediaQuery.of(context).size.width,
+            child: Center(
+              child: OutlineButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Mein Raum",
+                    // style: Theme.of(context).textTheme.headline5,
+                  ),
+                  borderSide: BorderSide(color: Colors.white),
+                  textColor: Colors.white,
+                  padding: EdgeInsets.symmetric(horizontal: 90.0)),
+            ),
+          ),
+          Positioned(
+            top: 90,
+            right: 0,
+            child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 8,
+                  mainAxisSpacing: 8,
+                  padding: EdgeInsets.all(10),
+                  children: [
+                    Card(
+                      elevation: 0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: AnimatedBackground(
+                          behaviour: RandomParticleBehaviour(
+                            options: ParticleOptions(
+                              spawnMinSpeed: 1.2,
+                              spawnMaxSpeed: 5.5,
+                              baseColor: Color(0xff81B9BF),
+                              minOpacity: 0.1,
+                              maxOpacity: 0.9,
+                            ),
+                          ),
+                          vsync: this,
+                          child: Container(),
+                        ),
+                      ),
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.white70, width: 1),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    Card(
+                      elevation: 0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: AnimatedBackground(
+                          behaviour: RandomParticleBehaviour(
+                            options: ParticleOptions(
+                              spawnMinSpeed: 1.2,
+                              spawnMaxSpeed: 5.5,
+                              baseColor: Color(0xff81B9BF),
+                              minOpacity: 0.1,
+                              maxOpacity: 0.9,
+                            ),
+                          ),
+                          vsync: this,
+                          child: Container(),
+                        ),
+                      ),
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.white70, width: 1),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    Card(
+                      elevation: 0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: AnimatedBackground(
+                          behaviour: RandomParticleBehaviour(
+                            options: ParticleOptions(
+                              spawnMinSpeed: 1.2,
+                              spawnMaxSpeed: 5.5,
+                              baseColor: Color(0xff81B9BF),
+                              minOpacity: 0.1,
+                              maxOpacity: 0.9,
+                            ),
+                          ),
+                          vsync: this,
+                          child: Container(),
+                        ),
+                      ),
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.white70, width: 1),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    Card(
+                      elevation: 0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: AnimatedBackground(
+                          behaviour: RandomParticleBehaviour(
+                            options: ParticleOptions(
+                              spawnMinSpeed: 1.2,
+                              spawnMaxSpeed: 5.5,
+                              baseColor: Color(0xff81B9BF),
+                              minOpacity: 0.1,
+                              maxOpacity: 0.9,
+                            ),
+                          ),
+                          vsync: this,
+                          child: Container(),
+                        ),
+                      ),
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.white70, width: 1),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    Card(
+                      elevation: 0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: AnimatedBackground(
+                          behaviour: RandomParticleBehaviour(
+                            options: ParticleOptions(
+                              spawnMinSpeed: 1.2,
+                              spawnMaxSpeed: 5.5,
+                              baseColor: Color(0xff81B9BF),
+                              minOpacity: 0.1,
+                              maxOpacity: 0.9,
+                            ),
+                          ),
+                          vsync: this,
+                          child: Container(),
+                        ),
+                      ),
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.white70, width: 1),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    Card(
+                      elevation: 0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: AnimatedBackground(
+                          behaviour: RandomParticleBehaviour(
+                            options: ParticleOptions(
+                              spawnMinSpeed: 1.2,
+                              spawnMaxSpeed: 5.5,
+                              baseColor: Color(0xff81B9BF),
+                              minOpacity: 0.1,
+                              maxOpacity: 0.9,
+                            ),
+                          ),
+                          vsync: this,
+                          child: Container(),
+                        ),
+                      ),
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.white70, width: 1),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ],
+                )),
+          )
+        ]));
   }
 }
